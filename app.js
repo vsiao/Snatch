d62cb4e8d460be821116017088572b84edad9185
Vincent Siao <vincentsiao@gmail.com>
2013-01-05 08:06:30 -0500

    room listing on index

--- app.js
+++ app.js
@@ -14,12 +14,36 @@ var server = app.listen(port, function() {
   console.log("Listening on port %d in %s mode", port, app.settings.env);
 });
 
+var players = {};
+
+function buildRoomData() {
+  var rooms = [];
+  var i, room, room_id, clients;
+  for (room_id in io.sockets.manager.rooms) {
+    room = {room_id: room_id, users: []};
+    clients = io.sockets.manager.rooms[room_id];
+    for (i = 0; i < clients.length; ++i) {
+      if (players.hasOwnProperty(clients[i]) &&
+          players[clients[i]].status != 'waiting') {
+        room.users.push(players[clients[i]].playername);
+      }
+    }
+    rooms.push(room);
+  }
+  return rooms;
+}
+
 var io = SocketIO.listen(server);
 io.sockets.on('connection', function(socket) {
-  socket.emit('hello', {data: 'world'});
-  socket.on('goodbye', function(data) {
-    console.log(data);
+  socket.on('updatestatus', function(data) {
+    socket.join(data.room);
+    socket.broadcast.emit('update:rooms', {rooms: buildRoomData()});
+    players[socket.id] = data;
   });
+});
+
+app.get('/', function(req, res) {
+  res.render('index', {rooms: buildRoomData()});
 });
 
 app.get('/rooms/:room_id', function(req, res) {
